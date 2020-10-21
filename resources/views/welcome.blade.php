d345efe0c8b8e63719892302b020f7d30f59eee7
0gravity <0gravity000@gmail.com>
2020-10-21 09:52:52 +0900

    WelcomeページのヘッダーにBootstrapのNavbarを適用 webpack.mix.jsにカスタムcssを追加
    Changes to be committed:
        modified:   public/mix-manifest.json
        modified:   resources/css/style.css
        modified:   resources/views/welcome.blade.php
        modified:   webpack.mix.js

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,24 +20,41 @@
         <div class="container-fluid">
             <div class="row header">
                 <div class="col">
-                    <a href="/">Demo Shop1（ロゴ）</a>
+                    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                        <a class="navbar-brand" href="/">Demo Shop1（ロゴ）</a>
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul class="navbar-nav mr-auto">
+                                <li class="nav-item active">
+                                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Link</a>
+                                </li>
+                                @if (Route::has('login'))
+                                    @auth
+                                        <li class="nav-item">
+                                        <a class="nav-link" href="{{ url('/home') }}">Home</a>
+                                        </li>
+                                    @else
+                                        <li class="nav-item">
+                                        <a class="nav-link" href="{{ route('login') }}">Login</a>
+                                        </li>
+                                        @if (Route::has('register'))
+                                            <li class="nav-item">
+                                            <a class="nav-link" href="{{ route('register') }}">Register</a>
+                                            </li>
+                                        @endif
+                                    @endauth
+                                @endif
+                            </ul>
+                        </div>
+                    </nav>
                 </div>
                 <div class="col">
-                    <div class="flex-center position-ref full-height">
-                        @if (Route::has('login'))
-                            <div class="top-right links">
-                                @auth
-                                    <a href="{{ url('/home') }}">Home</a>
-                                @else
-                                    <a href="{{ route('login') }}">Login</a>
-
-                                    @if (Route::has('register'))
-                                        <a href="{{ route('register') }}">Register</a>
-                                    @endif
-                                @endauth
-                            </div>
-                        @endif
-                    </div>
                 </div>
             </div>
             <div class="row content1">
